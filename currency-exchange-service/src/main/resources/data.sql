620f06c7376d2bb5c1b30ae01226aabe38700699
Diego <diegoalencar.jacober@gmail.ocm>
2023-09-09 11:10:14 -0300

    service currency exchange

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id, currency_from, currency_to,conversion_multiple, environment)
+values(1000, 'EUR', 'BRL', 65, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to,conversion_multiple, environment)
+values(1001, 'USD', 'BRL', 80, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to,conversion_multiple, environment)
+values(1002, 'AUD', 'BRL', 40, '');
