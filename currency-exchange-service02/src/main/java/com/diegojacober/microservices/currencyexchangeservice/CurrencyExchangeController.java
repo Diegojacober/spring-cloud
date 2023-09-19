7d83cb7c089e85f9cdcec416d23f78f3ff7ff8db
Diego Alencar Jacober <57761933898>
2023-09-19 16:51:11 -0300

    Connecting Currency conversion and API gateway

--- currency-exchange-service02/src/main/java/com/diegojacober/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service02/src/main/java/com/diegojacober/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,8 @@
 package com.diegojacober.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository repository;
 
@@ -19,6 +22,9 @@ public class CurrencyExchangeController {
     
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retriveExchangeValue( @PathVariable String from, @PathVariable String to) {
+
+        logger.info("retriveExchangeValue called with"+ from +" to " + to);
+        
        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
        if (currencyExchange == null) {
